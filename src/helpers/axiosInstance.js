eca97ffff013961e7a7fdaa88ed81326f299de3c
PyRoh <117882176+PythonRoh@users.noreply.github.com>
2025-07-29 16:08:19 +0530

    refactor: Simplify axios instance creation and ensure token injection in headers

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,9 +1,23 @@
 import axios from "axios";
-import {BASE_URL} from "../constants.js"
+import { BASE_URL } from "../constants.js";
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // if using cookies; not needed for token-based unless cross-origin
+});
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+// ✅ Add interceptor to inject token in header
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // or from Redux/store
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
